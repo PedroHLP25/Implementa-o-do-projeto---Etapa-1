35b19d43774dface76c6f7fce081f206b45c7908
Marcelo Augusto <marcelo513.ma@gmail.com>
2024-09-13 22:06:44 -0300

    Funcional

--- main.py
+++ main.py
@@ -199,27 +199,3 @@ def ver_jogador(nome: str):
         "exercitos": jogador.exercitos,
         "cartas": jogador.cartas
     }
-
-
-# app = FastAPI()
-
-# jogo = Jogo()
-# jogador1 = Jogador("Edson", "Vermelho")
-# jogador2 = Jogador("Marcelo", "Azul")
-# jogador3 = Jogador("Pedro", "Verde")
-
-# jogo.adicionar_jogador(jogador1)
-# jogo.adicionar_jogador(jogador2)
-# jogo.adicionar_jogador(jogador3)
-
-# jogo.definir_ordem_jogadores()
-# jogo.distribuir_territorios()
-# jogo.distribuir_objetivos()
-# jogo.iniciar_rodada()
-
-
-# jogo.distribuir_cartas()
-
-
-# with open('dados.json', 'w') as f:
-#     f.write(jogo.gerar_json())
